0338230f3721e152be0a8719c0780a36da0178d2
unknown <wyyuddin@gmail.com>
2021-04-26 11:13:06 +0700

    belajar php

---
+++ belajar.php
@@ -0,0 +1,21 @@
+<?php
+echo "<h1>Hallo, Selamat datang di duniaku</h1>";
+$nama = "Waliyyuddin";
+$umur = "19th";
+$alamat = "Jl. Tamansari no. 96A/56";
+// $anggota = array("Aku","Kamu","Kita");
+$heh = "Terima kasih";
+// $heh = "Nuhun";
+"$nama";
+"<br/>";
+"$umur";
+"<br/>";
+"$alamat";
+"<br/>";
+"$heh";
+echo "Halo guys, perkenalkan nama saya ".$nama." saya berumur ".$umur." saya beralamat tinggal di ".$alamat;
+// echo "<br/>";
+// echo "Aku mau kita termasuk dalam sebuah kelompok, yaitu ".$anggota;
+echo "<br/>";
+echo "$heh";
+?>
