00c4ff2979399f2fb963abc703e0f869974e4c52
unknown <wyyuddin@gmail.com>
2021-07-10 11:19:13 +0700

    membuat halaman web dinamis

---
+++ dinamis_php/halaman/home.php
@@ -0,0 +1,4 @@
+<div class="halaman">
+    <h2>Halaman Depan</h2>
+    <p>Selamat datang di www.waliyyuddin.com. Ini merupakan halaman depan pada demo Membuat Halaman Web Dinamis Dengan PHP.</p>
+</div>
---
+++ dinamis_php/halaman/tentang.php
@@ -0,0 +1,5 @@
+<div class="halaman">
+    <h2>Tentang Kami</h2>
+    <p>Ini adalah halaman tentang kami</p>
+    <p>Waliyyuddin.com adalah situs yang berisi mengenai brand yang sangat populer pada zaman sekarang hingga zaman-zaman yang akan mendatang.</p>
+</div>
